dbd747cf7cd567b9694a4e88cee527e5395c3461
KristinaXXX <107407324+KristinaXXX@users.noreply.github.com>
2023-11-26 11:03:56 +0500

    Безопастность №2

--- Сhucknorris/Сhucknorris/SceneDelegate.swift
+++ Сhucknorris/Сhucknorris/SceneDelegate.swift
@@ -19,7 +19,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let scene = (scene as? UIWindowScene) else { return }
         
         let window = UIWindow(windowScene: scene)
-        
         //RealmService.deleteRealm()
         let mainCoordinator = MainCoordinator()
         
